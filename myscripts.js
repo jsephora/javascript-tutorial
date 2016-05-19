752650978bf218b31a42819bc1e2918029d40fbc
Joseph Sephora <josephsephora@graphicdesignservices.ato.gov.au>
2016-05-19 14:20:07 +1000

    first or second commit

---
+++ myscripts.js
@@ -0,0 +1,11 @@
+ $(document).ready(function() {
+
+  $('button').click(function(){    
+    $('.navbar').fadeToggle("slow");
+  });
+  
+  $('button').click(function(){    
+    $('button').slideDown(3000);
+  });
+  
+});
